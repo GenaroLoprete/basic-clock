56c9cb3406796e0530328de5001a631a125f7e39
GenaroLoprete <gena.loprete@gmail.com>
2021-03-05 21:38:30 -0300

    colors in the displays

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A basic clock to know the hour in Spain and Uruguay
 
 //TODO
 //Add some colours to the console
+//Add horario de drake, una hora menos
+//drake rosa
+//rojo españa
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const CronJob = require('cron').CronJob;
 const moment = require('moment-timezone');
+const colors = require('colors');
 const dateFormat = 'D/M/YY HH:mm';
 //function for log the times, simple console.log
 const logTimes = () => {
     console.clear(); //First we clear the console
 
-    console.log('UY Time');
-    console.log(getTime('UY'));
+    console.log('UY Time'.blue);
+    console.log(getTime('UY').toString().blue);
 
-    console.log('ESP Time');
-    console.log(getTime('ESP'));
+    console.log('ESP Time / Barcelona'.red);
+    console.log(getTime('ESP-1').toString().red);
 
+    console.log('ESP Time / Africa (Drake)'.cyan);
+    console.log(getTime('ESP-2').toString().cyan);
 }
 
 ///get the time by some code
@@ -21,7 +24,8 @@ const getTime = (timeCode) => {
     timeCode = '' + timeCode.toUpperCase();
     const date = moment();
     if (timeCode === 'UY') return moment(date).tz('America/Montevideo').format(dateFormat);
-    if (timeCode === 'ESP') return moment(date).tz('Europe/Madrid').format(dateFormat);
+    if (timeCode === 'ESP-1') return moment(date).tz('Europe/Madrid').format(dateFormat);
+    if (timeCode === 'ESP-2') return moment(date).tz('Europe/Madrid').subtract(1, 'hours').format(dateFormat);
 }
 
 //we want to update every minute
